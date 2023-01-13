15e0e24f42c8eb9e5ad97fb1ace0294e1253be4f
amansoni <amansoni2041999@gmail.com>
2023-01-14 00:19:41 +0530

    performing address book program

---
+++ Review/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            if(i<1)
+            {
+                Console.WriteLine("The number is prime number" );
+
+            }
+
+            else if(i==0)
+            {
+                Console.WriteLine("the number is not prime");
+            }
+        }
+    }
+}
