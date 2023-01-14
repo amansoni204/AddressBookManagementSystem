3fea6f66223972f38106f5e3316941db1e6913cb
amansoni <amansoni2041999@gmail.com>
2023-01-14 23:05:26 +0530

    performing address book program

--- AddressBook456/Program.cs
+++ AddressBook456/Program.cs
@@ -8,8 +8,6 @@ namespace AddressBook456
 {
     internal class Program
     {
-
-
         public string first_Name { get; set; }
         public string last_Name { get; set; }
         public string present_Address { get; set; }
@@ -18,9 +16,11 @@ namespace AddressBook456
         public string email_Id { get; set; }
         public string phone_Number { get; set; }
         public string zip { get; set; }
+        public bool Index { get; internal set; }
+        public int Id { get; internal set; }
         public override string ToString()
         {
             return "FirstName: " + this.first_Name + " \nLastName: " + this.last_Name + " \nPresent Address: " + this.present_Address + " \nCity: " + this.city + " \nState: " + this.state + " \nZipcode: " + this.zip + " \nPhoneNumber: " + this.phone_Number + " \nEmail: " + this.email_Id;
+
         }
-    }
 }
